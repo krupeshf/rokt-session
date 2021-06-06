1214af8b85c89bc3b63233be8922f45d88507cd5
Krupesh <krupesh.faldu@clover.com>
2021-06-06 16:45:22 -0400

    test: session response class

--- src/test/java/com/krupesh/rokt/session/SessionResponseTest.java
+++ src/test/java/com/krupesh/rokt/session/SessionResponseTest.java
@@ -1,14 +1,42 @@
 package com.krupesh.rokt.session;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.DateTimeException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SessionResponseTest {
   @Test
-  public void parseSessionDetails() {
-    SessionResponse response = SessionResponse.fromString("2020-12-04T11:14:23Z jane.doe@email.com 2f31eb2c-a735-4c91-a122-b3851bc87355");
-    Assertions.assertEquals("2020-12-04T11:14:23Z", response.getEventTime().toString());
-    Assertions.assertEquals("jane.doe@email.com", response.getEmail());
-    Assertions.assertEquals("2f31eb2c-a735-4c91-a122-b3851bc87355", response.getSessionId().toString());
+  public void givenValidLine_parseSession() {
+    SessionResponse response = SessionResponse.fromString(
+        "2020-12-04T11:14:23Z jane.doe@email.com 2f31eb2c-a735-4c91-a122-b3851bc87355");
+    assertEquals("2020-12-04T11:14:23Z", response.getEventTime().toString());
+    assertEquals("jane.doe@email.com", response.getEmail());
+    assertEquals("2f31eb2c-a735-4c91-a122-b3851bc87355", response.getSessionId().toString());
+  }
+
+  @Test
+  public void givenInvalidDate_throwDateTimeParseException() {
+    assertThrows(DateTimeException.class, () -> {
+      SessionResponse.fromString(
+          "2020-13-04T11:14:23Z jane.doe@email.com 2f31eb2c-a735-4c91-a122-b3851bc87355");
+    });
+  }
+
+  @Test
+  //ToDo: we should have email validation
+  public void givenInvalidEmail_acceptIt() {
+    assertDoesNotThrow(() -> {
+      SessionResponse.fromString(
+          "2020-12-04T11:14:23Z invalid.email 2f31eb2c-a735-4c91-a122-b3851bc87355");
+    });
+  }
+
+  @Test
+  public void givenInvalidSessionid_throwIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      SessionResponse.fromString("2020-12-04T11:14:23Z jane.doe@email.com f31eb2c-b3851bc87355");
+    });
   }
 }
